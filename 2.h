d794dadca8f769b895bc3685980399b8b0ddece1
nicolepavlovaa <niki.0199@gmail.com>
2019-03-01 15:58:24 +0200

    Create 2.h

---
+++ 2.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class ImaginaryNumber
+{
+private:
+    int a;
+    
+public:
+    ImaginaryNumber(int a, int b);
+    int getA();
+    void setA(int a);
+    void print();
+    ImaginaryNumber add(number2);
+    ImaginaryNumber multiply(number2);
+    ImaginaryNumber divide(number2);
+    
+};
