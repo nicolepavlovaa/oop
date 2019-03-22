9cdc16571e4f5e9850f386d1d970f02103f9dca2
nicolepavlovaa <niki.0199@gmail.com>
2019-03-22 23:48:40 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,26 +1,30 @@
-#pragma once
-#include <iostream>
+
+#ifndef __PLAYER_H
+#define __PLAYER_H
 class Student
 {
 	char * name;
 	int fn;
-	int course; 
+	int course;
 
 public:
-	Student();
-	Student(char * name, int fn, int course);
+	Student(const char * name = "Anonymous", int fn = 0, int course = 0);
 	Student(Student& other);
 	Student& operator=(Student& other);
 	bool operator!=(Student & other);
 	bool operator==(Student & other);
+	friend std::istream& operator>>(std::istream &is, Student &other);
+	friend std::ostream& operator<<(std::ostream &os, const Student &other);
 	~Student();
 
-	void setName(char * n);
+	void setName(const char * n);
 	void setFn(int fn);
 	void setCourse(int course);
-	char * getName();
-	int getFn();
-	int getCourse();
+
+	char * getName() const;
+	int getFn() const;
+	int getCourse() const;
 
 	void print();
 };
+#endif
